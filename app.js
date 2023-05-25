12b8bad6dcb2c0e46b27591dbc01c564c4cd43c3
Shrutimendapara6012 <134376501+Shrutimendapara6012@users.noreply.github.com>
2023-05-25 15:32:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const app = express()
+const PORT = 9000
+const hbs = require("hbs")
+const path = require("path")
+
+const weather = require("./util/weather")
+const geocode = require("./util/geocode")
+
+
+const viewPath= path.join(__dirname,"./templetes/views")
+
+const partialPath = path.join(__dirname,"./templetes/partials")
+
+const publicpath = path.join(__dirname,"./public")
+
+app.set("view engine","hbs")
+app.set("views",viewPath)
+hbs.registerPartials(partialPath)
+app.use(express.static(publicpath))
+
+app.get("/",(req,resp)=>{
+    resp.render("index")
+})
+app.get("/home",(req,resp)=>{
+    resp.render("home")
+})
+app.get("/contact",(req,resp)=>{
+    resp.render("contact")
+})
+
+
+
+app.get("/about",(req,resp)=>{
+    resp.render("about")
+})
+
+app.get("/weather",(req,resp)=>{
+    resp.render("weather")
+})
+
+
+app.get("/task",(req,resp)=>{
+    resp.render("task")
+})
+
+app.get("/currentweather",(req,resp)=>{
+
+   const location =  req.query.location;
+
+    geocode.geocodedata(location,(data,err)=>{
+        weather.weatherdata(data.lat,data.lng,(result,err)=>{
+           
+            resp.send({
+                city : result.city,
+                lat : data.lat,
+                lng : data.lng,
+                temp : result.temp,
+                pressure : result.pressure,
+                humidity : result.humidity
+            })
+
+        })
+    })
+
+
+
+})
+
+app.listen(PORT,(req,resp)=>{
+    console.log("server running on port :"+ PORT);
+})
